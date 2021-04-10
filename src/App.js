856536e9d184a2e025ab6806a071aaaeafaa5d17
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-04-10 11:28:45 +0100

    5.2: used localStorage, added logout

--- src/App.js
+++ src/App.js
@@ -15,18 +15,36 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (e) {
       console.log(e)
     }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
   }
 
   // Login form
@@ -61,7 +79,11 @@ const App = () => {
   const blogList = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
+      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
