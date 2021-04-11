2de439ad720e6b750a4ee1195b5b6e55788ee4e1
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-04-11 13:03:48 +0100

    5.9: Blogs are sorted by likes

--- src/App.js
+++ src/App.js
@@ -151,8 +151,10 @@ const App = () => {
         <BlogForm addBlog={addBlog} />
       </Togglable>
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleLikes={incrementLikes} />
+      {[...blogs]
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} handleLikes={incrementLikes} />
       )}
     </div>
   )
