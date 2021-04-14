e2eb550999dd3bebb822532a39a88e9956c67af1
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-04-14 23:30:16 +0100

    5.20: cypres test: blog can be liked

--- cypress/integration/bloglist.spec.js
+++ cypress/integration/bloglist.spec.js
@@ -7,6 +7,18 @@ Cypress.Commands.add('login', ({ username, password }) => {
   })
 })
 
+Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: { title, author, url },
+    headers: {
+      'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedInUser')).token}`
+    }
+  })
+  cy.visit('http://localhost:3001')
+})
+
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
@@ -55,10 +67,25 @@ describe('Blog app', function () {
       cy.get('#title').type('Blog Title')
       cy.get('#author').type('Blog author')
       cy.get('#url').type('www.blog-url.com/this-is-a-blog')
-
       cy.get('#create-button').click()
 
       cy.contains('Blog Title Blog author')
+    })
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'Test Blog Title',
+          author: 'Firstname Lastname',
+          url: 'www.some-url.com',
+        })
+      })
+
+      it('it can be liked', function () {
+        cy.get('.viewButton').click()
+        cy.get('.likeButton').click()
+        cy.get('.likesDiv').should('contain', 'likes 1')
+      })
     })
   })
 
